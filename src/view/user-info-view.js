3110fc02a78b3363590f2287325e9d5f318123e5
Klacc-Neitral <92113445+Klacc-Neitral@users.noreply.github.com>
2026-01-07 22:51:52 +0300

    Update user-info-view.js

--- src/view/user-info-view.js
+++ src/view/user-info-view.js
@@ -1,35 +1,40 @@
 import { AbstractComponent } from "../framework/view/abstract-component.js";
 
-
 function createUserInfoTemplate(user) {
+    const avatar = user.photo_url || 'https://via.placeholder.com/150';
+    const login = user.username || 'Не указан';
+    const firstName = user.first_name || 'Не указано';
+    const lastName = user.last_name || '';
+    const email = 'Скрыто Telegram'; // Telegram не отдает email автоматически
+
     return (
         `<section class="profile-details">
             <div class="profile-avatar">
                 <h2>Аватар профиля</h2>
-                <img src="${user.avatar}" alt="Аватар" class="avatar-image">
+                <img src="${avatar}" alt="Аватар" class="avatar-image" style="width: 150px; border-radius: 50%;">
                 <input type="file" id="avatar-input" accept="image/*" style="display: none">
                 <button class="btn btn-avatar">Выбрать аватар</button>
             </div>
 
             <form class="profile-form">
-                <h2>Личные данные</h2>
+                <h2>Личные данные (из Telegram)</h2>
                 <div class="form-group">
-                    <label>Логин</label>
-                    <input type="text" value="${user.login}" disabled>
+                    <label>ID пользователя</label>
+                    <input type="text" value="${user.id}" disabled>
                 </div>
                 <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" value="${user.email}" disabled>
+                    <label>Логин (@username)</label>
+                    <input type="text" value="${login}" disabled>
                 </div>
                 <div class="form-group">
                     <label>Имя</label>
-                    <input type="text" value="${user.name}" disabled>
+                    <input type="text" value="${firstName}" disabled>
                 </div>
                 <div class="form-group">
                     <label>Фамилия</label>
-                    <input type="text" value="${user.surname}" disabled>
+                    <input type="text" value="${lastName}" disabled>
                 </div>
-                <button type="button" class="btn btn-save" id="openModalBtn">Изменить</button>
+                <button type="button" class="btn btn-save" id="openModalBtn">Изменить (локально)</button>
             </form>
         </section>`
     );
@@ -39,21 +44,31 @@ export default class UserInfoView extends AbstractComponent {
     #user = null;
     _callback = {};
 
-    constructor(user) {
+    constructor() {
         super();
-        this.#user = user;
+        const tg = window.Telegram?.WebApp;
+        
+        if (tg?.initDataUnsafe?.user) {
+            this.#user = tg.initDataUnsafe.user;
+        } else {
+            this.#user = {
+                id: '0000',
+                first_name: 'Гость',
+                last_name: '',
+                username: 'guest',
+                photo_url: ''
+            };
+        }
     }
 
     get template() {
         return createUserInfoTemplate(this.#user);
     }
 
-
     setEditClickHandler(callback) {
         this._callback.editClick = callback;
         this.element.querySelector('#openModalBtn').addEventListener('click', this.#editClickHandler);
     }
-
 
     setAvatarClickHandler(callback) {
         this._callback.avatarClick = callback;
@@ -63,26 +78,24 @@ export default class UserInfoView extends AbstractComponent {
 
         avatarBtn.addEventListener('click', (evt) => {
             evt.preventDefault();
-            fileInput.click(); // Открываем окно выбора файла
+            fileInput.click();
         });
 
         fileInput.addEventListener('change', this.#avatarChangeHandler);
     }
-
 
     #editClickHandler = (evt) => {
         evt.preventDefault();
         this._callback.editClick();
     };
 
- 
     #avatarChangeHandler = (evt) => {
         const file = evt.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = () => {
-                // Передаем результат (base64 строку) в колбэк презентера
                 this._callback.avatarClick(reader.result);
+                this.element.querySelector('.avatar-image').src = reader.result;
             };
             reader.readAsDataURL(file);
         }

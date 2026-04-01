846638cc60b21e91eb1ef560082e075473b9b8dc
Elder_Emperor <92113445+Klacc-Neitral@users.noreply.github.com>
2026-04-01 20:16:09 +0300

    Update config.js

--- docs/src/config.js
+++ docs/src/config.js
@@ -1,6 +1,6 @@
 // Для ngrok замените значение на ваш публичный URL, например:
 // const SERVER_URL = "https://abc123.ngrok-free.app";
-const SERVER_URL = "http://127.0.0.1:8000";
+const SERVER_URL = "https://my-backend-qb82.onrender.com";
 
 export const APP_CONFIG = {
     serverUrl: SERVER_URL,

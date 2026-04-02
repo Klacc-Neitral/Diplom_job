b55d531c7ac9854e360383bbbc5ec8c31d46c7bb
Elder_Emperor <92113445+Klacc-Neitral@users.noreply.github.com>
2026-04-02 08:48:02 +0300

    Update main.js

--- docs/src/main.js
+++ docs/src/main.js
@@ -5,17 +5,39 @@ import ApiService from "./framework/api-service.js";
 import { initPlatform } from "./platform/index.js";
 import { APP_CONFIG } from "./config.js";
 
-const END_POINT = APP_CONFIG.apiBaseUrl;
-const bodyElement = document.querySelector("body");
-const userModel = new UserModel();
-const apiService = new ApiService(END_POINT, userModel);
-const coursesModel = new CoursesModel(apiService);
+async function startApp() {
+  try {
+    console.log("APP START");
 
-const platformUser = await initPlatform();
-const persistedUser = await apiService.syncUser(platformUser);
-userModel.setUser(persistedUser);
+    const END_POINT = APP_CONFIG.apiBaseUrl;
+    const bodyElement = document.querySelector("body");
 
-const profilePresenter = new ProfilePresenter(bodyElement, coursesModel, userModel, apiService);
+    const userModel = new UserModel();
+    const apiService = new ApiService(END_POINT, userModel);
+    const coursesModel = new CoursesModel(apiService);
 
-await coursesModel.init();
-profilePresenter.init();
+    const platformUser = await initPlatform();
+    console.log("platformUser:", platformUser);
+
+    const persistedUser = await apiService.syncUser(platformUser);
+    console.log("persistedUser:", persistedUser);
+
+    userModel.setUser(persistedUser);
+
+    const profilePresenter = new ProfilePresenter(
+      bodyElement,
+      coursesModel,
+      userModel,
+      apiService
+    );
+
+    await coursesModel.init();
+    profilePresenter.init();
+
+  } catch (e) {
+    console.error("APP ERROR:", e);
+    document.body.innerHTML = "<h1>ERROR</h1><pre>" + e + "</pre>";
+  }
+}
+
+startApp();
